3bac6e4b41afebe946de01e4b0b86317f0d23e4a
Komorebi <karan1284@naver.com>
2024-06-27 02:26:16 +0900

    first commit

---
+++ src/main/java/api/drunkhouse/DrunkHouseApplication.java
@@ -0,0 +1,13 @@
+package api.drunkhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrunkHouseApplication.class, args);
+    }
+
+}
