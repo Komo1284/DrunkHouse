c337c73dcf36711f63937d7f05055fa23653c1da
Komorebi <karan1284@naver.com>
2024-07-23 15:45:47 +0900

    Add WebConfig

---
+++ src/main/java/api/drunkhouse/config/WebConfig.java
@@ -0,0 +1,24 @@
+package api.drunkhouse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);  // allowCredentials가 true인 경우 "*" 사용 불가
+            }
+        };
+    }
+}
